9d30d2c31190c520dca3df2fd22ff194b3ea1978
Himanshu Harsora <iamhimu2809@gmail.com>
2026-02-16 19:58:22 +0530

    feat: implement User and Teacher classes with static method and logging functionality

---
+++ 10_classes_and_oops/staticProp.js
@@ -0,0 +1,28 @@
+class User{
+    constructor(username){
+        this.username = username;
+    }
+
+    logMe(){
+        console.log(`Username : ${this.username}`);
+    }
+
+    static createId(){
+        return '123'
+    }
+}
+
+const himanshu = new User("Himanshu")
+// console.log(himanshu.createId());
+
+class Teacher extends User{
+    constructor(username ,email){
+        super(username);
+        this.email = email;
+    }
+}
+
+const iphone = new Teacher("iphone", "iphone@gmail.com")
+iphone.logMe()
+console.log(iphone.createId());
+
